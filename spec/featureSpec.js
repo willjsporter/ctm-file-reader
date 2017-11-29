fb5b111e37b266202dd2c2ba34683ba27413acea
Will Porter <willjsporter@hotmail.co.uk>
2017-11-29 00:15:02 +0000

    full feature test passing including prime identifier

--- spec/featureSpec.js
+++ spec/featureSpec.js
@@ -15,7 +15,7 @@ describe("Feature test)", function(){
 
   var reader
   var wordCounter
-  var Primer
+  var primer
   var expectedOutput
 
   beforeEach(function(){
@@ -31,10 +31,25 @@ describe("Feature test)", function(){
       {word: "that", count: 1}, {word: "for", count: 1}, {word: "sure", count: 1},
     ]
     reader.formattedRead('/Users/will/Projects/JobApplications/CompareTheMarket/techtest/testFile.txt')
-    // var textFileString = reader.
     wordCounter.wordCountText(reader.text)
 
     expect(wordCounter.wordArray).toEqual(expectedOutput)
   });
+
+  it("takes the text file and processes it into objects that shows if occurrences of each word are prime", function(){
+    expectedOutput = [
+      {word: "here", count: 7, isPrime: true}, {word: "in", count: 2, isPrime: true}, {word: "this", count: 1, isPrime: false}, {word: "file", count: 2, isPrime: true},
+      {word: "is", count: 2, isPrime: true}, {word: "a", count: 1, isPrime: false}, {word: "test", count: 13, isPrime: true}, {word: "it", count: 2, isPrime: true},
+      {word: "has", count: 1, isPrime: false}, {word: "some", count: 1, isPrime: false}, {word: "good", count: 1, isPrime: false}, {word: "stuff", count: 1, isPrime: false},
+      {word: "that", count: 1, isPrime: false}, {word: "for", count: 1, isPrime: false}, {word: "sure", count: 1, isPrime: false},
+    ]
+    reader.formattedRead('/Users/will/Projects/JobApplications/CompareTheMarket/techtest/testFile.txt')
+    wordCounter.wordCountText(reader.text)
+    var words = wordCounter.wordArray
+    primer = new Primer(words)
+    primer.primeWordCountIdentifier()
+    expect(primer.wordCountList).toEqual(expectedOutput)
+  });
+
 
 });
